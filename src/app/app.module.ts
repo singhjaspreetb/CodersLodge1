a09451930e903ef5de60291fe55ad40fc7e04670
Jaspreet Singh <harshitbhattal786@gmail.com>
2020-10-22 23:12:26 +0530

    latest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Home2Component } from './pages/home2/home2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Home2Component
   ],
   imports: [
     BrowserModule,
